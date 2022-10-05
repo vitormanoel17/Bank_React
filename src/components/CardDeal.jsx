6c2f851fdaaed235180a2ec66d7238136c88e77f
Vitormanoel17 <vitormanoel.aquino@gmail.com>
2022-10-05 16:56:06 -0300

    [ADD]: Adicionando componentes que já foram produzidos

---
+++ src/components/CardDeal.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { card } from '../assets';
+import Button from './Button';
+
+const CardDeal = () => (
+    <section>
+      
+    </section>
+)
+
+export default CardDeal
